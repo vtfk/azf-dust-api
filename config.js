205eb343a613249819aecf46a520dff326d1f9a3
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-15 14:56:57 +0100

    Removed unused config entries

--- config.js
+++ config.js
@@ -8,8 +8,6 @@ module.exports = {
     audience: process.env.TOKEN_AUTH_AUD || CLIENT_ID || undefined // Application Client ID
   },
   SCRIPT_SERVICE_URL: process.env.SCRIPT_SERVICE_URL || 'http://localhost:3000',
-  PIFU_API_URL: process.env.PIFU_API_URL || 'https://pifu.api.no',
-  PIFU_API_JWT: process.env.PIFU_API_JWT || 'Super secret jwt secret',
   DUST_JWT_SECRET: process.env.DUST_JWT_SECRET || false,
   DEFAULT_CALLER: 'NoenAndré'
 }
