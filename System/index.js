0c83afe19b3934d69b7a48d719994a64a4065e0d
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-03-02 17:12:12 +0100

    Cleanup

--- System/index.js
+++ System/index.js
@@ -1,5 +1,6 @@
 const { DEFAULT_CALLER } = require('../config')
 const { logger } = require('@vtfk/logger')
+const withTokenAuth = require('../lib/auth/with-token-auth')
 const callHandler = require('../handlers/call-handlers')
 
 const handleSystem = async (context, req) => {
