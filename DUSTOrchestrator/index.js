428c087dbef30cca2a3ac8020c65776cece9a4a7
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-03-04 16:03:49 +0100

    Validated systems are executed first. User object are then populated with missing properties

--- DUSTOrchestrator/index.js
+++ DUSTOrchestrator/index.js
@@ -1,4 +1,6 @@
 const df = require('durable-functions')
+const { validate } = require('../lib/user-query')
+const updateUser = require('../lib/update-user')
 
 module.exports = df.orchestrator(function * (context) {
   const { body: { systems, user }, token } = context.df.getInput()
@@ -15,13 +17,45 @@ module.exports = df.orchestrator(function * (context) {
     }
   })
 
-  // start all system requests
-  systems.forEach(system => {
-    parallelTasks.push(context.df.callActivity('DUSTActivity', { instanceId, system, user, token }))
-  })
+  // determine if enough data is present to call all systems at once, or if extens and visma needs to be called first
+  const systemsValidation = validate(systems, user)
+  if (systemsValidation.filter(validation => !validation.execute).length === 0) {
+    console.log('Start all systems')
+    // start all system requests
+    systems.forEach(system => {
+      parallelTasks.push(context.df.callActivity('DUSTActivity', { instanceId, system, user, token }))
+    })
 
-  // wait for all system requests to finish
-  yield context.df.Task.all(parallelTasks)
+    // wait for all system requests to finish
+    yield context.df.Task.all(parallelTasks)
+  } else {
+    // start validate systems first, wait for them to finish and update user object with missing properties
+    systemsValidation.filter(validation => validation.execute).forEach(validation => {
+      parallelTasks.push(context.df.callActivity('DUSTActivity', {
+        instanceId,
+        system: validation.system,
+        user,
+        token
+      }))
+    })
+    
+    // wait for validated system requests to finish
+    yield context.df.Task.all(parallelTasks)
+
+    const updatedUser = updateUser(parallelTasks.map(task => task.result), user)
+
+    systemsValidation.filter(validation => !validation.execute).forEach(validation => {
+      parallelTasks.push(context.df.callActivity('DUSTActivity', {
+        instanceId,
+        system: validation.system,
+        user: updatedUser,
+        token
+      }))
+    })
+
+    // wait for non-validated system requests to finish
+    yield context.df.Task.all(parallelTasks)
+  }
 
   // update request with a finish timestamp
   const timestamp = new Date().toISOString()
