4b59812818978f6426fe3753caf1c09f06de7214
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-03-17 21:01:26 +0100

    Lagt til sjekk for fnr mot AD

--- systems/pifu/validator.js
+++ systems/pifu/validator.js
@@ -39,13 +39,30 @@ module.exports = (systemData, user, allData = false) => ([
     }
     return data.fnr.valid ? success(`Har gyldig ${data.fnr.type}`, data) : error(data.fnr.error, data)
   }),
-  test('pifu-04', 'Har gruppemedlemskap', 'Sjekker at det finnes gruppemedlemskap', () => {
+  test('pifu-04', 'Fødselsnummer er likt i AD', 'Sjekker at fødselsnummeret er likt i AD og Extens', () => {
+    if (!allData) return noData('Venter på data...')
+    if (!allData.ad) return error('Mangler AD-data', allData)
+
+    if (!hasData(systemData.person)) return error('Person-objekt mangler 🤭', systemData)
+    else if (!hasData(systemData.person.sourcedid)) return error('Person-objekt mangler source 🤭', systemData)
+    const data = {
+      pifu: {
+        id: Number.isInteger(systemData.person.sourcedid.id) ? systemData.person.sourcedid.id.toString() : systemData.person.sourcedid.id
+      },
+      ad: {
+        employeeNumber: allData.ad.employeeNumber
+      }
+    }
+    if (data.pifu.id === data.ad.employeeNumber) return success('Fødselsnummer er likt i AD og Extens', data)
+    else return error('Fødselsnummer er forskjellig i AD og Extens', data)
+  }),
+  test('pifu-05', 'Har gruppemedlemskap', 'Sjekker at det finnes gruppemedlemskap', () => {
     // TODO: Bør det sjekkes noe mere here? Er det noen ganger det er riktig at det ikke er noen gruppemedlemskap?
     const memberships = getMemberships(systemData.memberships)
     if (!memberships) return error('Har ingen gruppemedlemskap 🤭', systemData)
     else return success(`Har ${memberships.length} gruppemedlemskap`, memberships)
   }),
-  test('pifu-05', 'Har riktig rolletype', 'Sjekker at det er riktig rolletype i gruppemedlemskapene', () => {
+  test('pifu-06', 'Har riktig rolletype', 'Sjekker at det er riktig rolletype i gruppemedlemskapene', () => {
     const memberships = getMemberships(systemData.memberships)
     if (!memberships) return error('Har ingen gruppemedlemskap 🤭', systemData)
     const data = memberships.map(membership => ({ id: membership.sourcedid.id, type: membership.member.role['@_roletype'] }))
@@ -57,7 +74,7 @@ module.exports = (systemData, user, allData = false) => ([
       return hasData(wrongMemberships) ? error(`Har ${wrongMemberships.length} gruppemedlemskap med feil rolletype`, data) : success('Har riktig rolletype i alle gruppemedlemskapene', data)
     }
   }),
-  test('pifu-06', 'Gruppemedlemskapet er gyldig', 'Sjekker at gruppemedlemskapene ikke er avlsuttet', () => {
+  test('pifu-07', 'Gruppemedlemskapet er gyldig', 'Sjekker at gruppemedlemskapene ikke er avlsuttet', () => {
     const memberships = getMemberships(systemData.memberships)
     if (!memberships) return error('Har ingen gruppemedlemskap 🤭', systemData)
     const invalidMemberships = memberships.filter(item => !isWithinDaterange(item.member.role.timeframe.begin['#text'], item.member.role.timeframe.end['#text']))
