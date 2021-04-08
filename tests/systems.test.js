b9f6a73a90a74446bb96c5b62fe020a56815fd3d
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-04-08 21:17:51 +0200

    Testene må returnere et result objekt

--- tests/systems.test.js
+++ tests/systems.test.js
@@ -1233,7 +1233,7 @@ systems.forEach(system => {
   test(`Sjekker at alle ${system}-tester returnerer et objekt med riktig innhold`, () => {
     const tests = validator(data, mockUser)
     expect(Array.isArray(tests)).toBe(true)
-    const wrong = tests.filter(item => typeof item !== 'object' || (!item.id || !item.title || !item.description || !item.result || !item.result.status || !item.result.message))
+    const wrong = tests.filter(item => typeof item !== 'object' || (!item.id || !item.title || !item.description || !item.result || typeof item.result !== 'object' || !item.result.status || !item.result.message))
     expect(wrong.length).toBe(0)
   })
 
