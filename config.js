82d110973ad8a54d6596fe371b42648e42acad01
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-04-22 11:18:49 +0200

    Lagt til URL for beta

--- config.js
+++ config.js
@@ -13,7 +13,8 @@ module.exports = {
       GRANT_TYPE: process.env.GRAPH_GRANT_TYPE || 'client_credentials',
       SCOPE: process.env.GRAPH_SCOPE || 'https://graph.microsoft.com/.default'
     },
-    URL: process.env.GRAPH_API || 'https://graph.microsoft.com/v1.0/users',
+    URL: process.env.GRAPH_API || 'https://graph.microsoft.com/v1.0',
+    URL_BETA: process.env.GRAPH_API_BETA || 'https://graph.microsoft.com/beta',
     DEFAULT_USER_PROPERTIES: (process.env.GRAPH_USER_PROPERTIES && process.env.GRAPH_USER_PROPERTIES.split(',')) || '*'
   },
   RETRY_WAIT: (process.env.RETRY_WAIT && Number.parseInt(process.env.RETRY_WAIT)) || 3000,
