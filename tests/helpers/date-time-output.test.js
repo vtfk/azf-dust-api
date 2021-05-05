8783aa802b1fbd56daaed5252cac50810e5ff4ee
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-05-05 16:46:50 +0200

    Støtte UTC og GMT

--- tests/helpers/date-time-output.test.js
+++ tests/helpers/date-time-output.test.js
@@ -3,10 +3,10 @@ const date = new Date('2021-05-05T04:35:03.0852857+02:00')
 
 test('Prettified date is padded correctly', () => {
   const prettifiedDate = prettifyDate(date)
-  expect(prettifiedDate).toBe('05.05.2021 04:35:03')
+  expect(prettifiedDate).toMatch(/\d{2}.\d{2}.\d{4} \d{2}:\d{2}:\d{2}/)
 })
 
 test('AzureAD date is padded correctly', () => {
   const prettifiedDate = azureADDate(date)
-  expect(prettifiedDate).toBe('2021-05-05')
+  expect(prettifiedDate).toMatch(/\d{4}-\d{2}-\d{2}/)
 })
