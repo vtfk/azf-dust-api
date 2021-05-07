520d17459c0b39069374a7425f17cc894779a6fb
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-05-07 12:41:21 +0200

    Omgjør amerikansk datoformat til norsk datoformat

--- tests/helpers/date-time-output.test.js
+++ tests/helpers/date-time-output.test.js
@@ -1,13 +1,8 @@
-const { prettifyDateToLocaleString, prettifyDate, azureADDate } = require('../../lib/helpers/date-time-output')
+const { prettifyDateToLocaleString, azureADDate } = require('../../lib/helpers/date-time-output')
 const date = new Date('2021-05-05T04:35:03.0852857+02:00')
 
 test('Prettified locale string is padded correctly', () => {
   const prettifiedDate = prettifyDateToLocaleString(date)
-  expect(prettifiedDate).toMatch(/\d{2}.\d{2}.\d{4} \d{2}:\d{2}:\d{2}/)
-})
-
-test('Prettified date is padded correctly', () => {
-  const prettifiedDate = prettifyDate(date)
   expect(prettifiedDate).toMatch(/\d{2}.\d{2}.\d{4} \d{2}:\d{2}:\d{2}/)
 })
 
