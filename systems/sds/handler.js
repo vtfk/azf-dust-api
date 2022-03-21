c905557a15e1e6b0f1d7e0a04af8b0c549eb1e7e
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-03-21 13:14:07 +0100

    Get SDS user from mongo

--- systems/sds/handler.js
+++ systems/sds/handler.js
@@ -1,14 +1,7 @@
 const { logger } = require('@vtfk/logger')
-const handleData = require('../../lib/handle-data')
+const { getSDSUser } = require('../../lib/mongo/handle-mongo')
 const getResponse = require('../../lib/get-response-object')
 const HTTPError = require('../../lib/http-error')
-
-const getData = async (caller, data) => {
-  const method = 'get'
-  const fileName = 'Get-DUSTSds.ps1'
-
-  return await handleData(caller, 'sds', method, fileName, data)
-}
 
 module.exports = async (caller, params) => {
   const { samAccountName, userPrincipalName, type } = params
@@ -24,22 +17,18 @@ module.exports = async (caller, params) => {
 
   if (samAccountName !== undefined) {
     logger('info', ['sds', 'samAccountName', samAccountName, type])
-    const data = await getData(caller, {
-      samAccountName,
-      type
-    })
-    logger('info', ['sds', 'samAccountName', samAccountName, type, 'data', 'received', Array.isArray(data) ? data.length : 1])
-    return getResponse(data)
+    const data = await getSDSUser({ samAccountName })
+    const sds = data ? data.sds : []
+    logger('info', ['sds', 'samAccountName', samAccountName, type, 'data', 'received', sds.length])
+    return getResponse(sds)
   }
 
   if (userPrincipalName !== undefined) {
     logger('info', ['sds', 'userPrincipalName', userPrincipalName, type])
-    const data = await getData(caller, {
-      userPrincipalName,
-      type
-    })
-    logger('info', ['sds', 'userPrincipalName', userPrincipalName, type, 'data', 'received', Array.isArray(data) ? data.length : 1])
-    return getResponse(data)
+    const data = await getSDSUser({ userPrincipalName })
+    const sds = data ? data.sds : []
+    logger('info', ['sds', 'userPrincipalName', userPrincipalName, type, 'data', 'received', sds.length])
+    return getResponse(sds)
   }
 
   throw new HTTPError(400, 'Missing required parameter(s)', {
