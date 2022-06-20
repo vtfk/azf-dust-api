5e358e766bbe778837770da0fc9ba2e316a74d6c
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-06-20 19:22:32 +0200

    Use `title` instead of `type` aswell as support properties from `ad` and `aad`

--- tests/helpers/is-type.test.js
+++ tests/helpers/is-type.test.js
@@ -1,29 +1,47 @@
 const { isApprentice, isOT, isEmployee, isStudent, isTeacher } = require('../../lib/helpers/is-type')
 
-const apprenticeType = 'Lærling'
-const otType = 'Elev-OT'
+const apprenticeTitle = 'Lærling'
+const otTitle = 'Elev-OT'
 const employee = '@vtfk.no'
-const studentType = 'Elev'
+const studentTitle = 'Elev'
 
-describe('isApprentice', () => {
+describe('isApprentice : AD', () => {
   test('user object not set', () => {
     expect(isApprentice()).toBe(false)
   })
 
-  test('type not set', () => {
+  test('title not set', () => {
     expect(isApprentice({ displayName: 'Test' })).toBe(false)
   })
 
-  test('type set to an empty string', () => {
-    expect(isApprentice({ type: '' })).toBe(false)
+  test('title set to an empty string', () => {
+    expect(isApprentice({ title: '' })).toBe(false)
   })
 
-  test.each([otType, studentType])('type set as %p should be false', type => {
-    expect(isApprentice({ type })).toBe(false)
+  test.each([otTitle, studentTitle])('title set as %p should be false', title => {
+    expect(isApprentice({ title })).toBe(false)
   })
 
-  test(`type set as "${apprenticeType}" should be true`, () => {
-    expect(isApprentice({ type: apprenticeType })).toBe(true)
+  test.each(['login', ''])(`domain set as %p and title set as "${apprenticeTitle}" should be false`, domain => {
+    expect(isApprentice({ domain, title: apprenticeTitle })).toBe(false)
+  })
+
+  test(`title set as "${apprenticeTitle}" and domain set as "skole" should be true`, () => {
+    expect(isApprentice({ domain: 'skole', title: apprenticeTitle })).toBe(true)
+  })
+})
+
+describe('isApprentice : AAD', () => {
+  test.each([otTitle, studentTitle])('jobTitle set as %p should be false', jobTitle => {
+    expect(isApprentice({ jobTitle })).toBe(false)
+  })
+
+  test.each(['DC=login', ''])(`onPremisesDistinguishedName includes %p and jobTitle set as "${apprenticeTitle}" should be false`, onPremisesDistinguishedName => {
+    expect(isApprentice({ onPremisesDistinguishedName, jobTitle: apprenticeTitle })).toBe(false)
+  })
+
+  test(`jobTitle set as "${apprenticeTitle}" and onPremisesDistinguishedName includes "DC=skole" should be true`, () => {
+    expect(isApprentice({ onPremisesDistinguishedName: 'DC=skole', jobTitle: apprenticeTitle })).toBe(true)
   })
 })
 
@@ -32,20 +50,38 @@ describe('isOT', () => {
     expect(isOT()).toBe(false)
   })
 
-  test('type not set', () => {
+  test('title not set', () => {
     expect(isOT({ displayName: 'Test' })).toBe(false)
   })
 
-  test('type set to an empty string', () => {
-    expect(isOT({ type: '' })).toBe(false)
+  test('title set to an empty string', () => {
+    expect(isOT({ title: '' })).toBe(false)
   })
 
-  test.each([apprenticeType, studentType])('type set as %p should be false', type => {
-    expect(isOT({ type })).toBe(false)
+  test.each([apprenticeTitle, studentTitle])('title set as %p should be false', title => {
+    expect(isOT({ title })).toBe(false)
   })
 
-  test(`type set as "${otType}" should be true`, () => {
-    expect(isOT({ type: otType })).toBe(true)
+  test.each(['login', ''])(`domain set as %p and title set as "${otTitle}" should be false`, domain => {
+    expect(isApprentice({ domain, title: otTitle })).toBe(false)
+  })
+
+  test(`title set as "${otTitle}" and domain set as "skole" should be true`, () => {
+    expect(isOT({ domain: 'skole', title: otTitle })).toBe(true)
+  })
+})
+
+describe('isOT : AAD', () => {
+  test.each([apprenticeTitle, studentTitle])('jobTitle set as %p should be false', jobTitle => {
+    expect(isOT({ jobTitle })).toBe(false)
+  })
+
+  test.each(['DC=login', ''])(`onPremisesDistinguishedName includes %p and jobTitle set as "${otTitle}" should be false`, onPremisesDistinguishedName => {
+    expect(isOT({ onPremisesDistinguishedName, jobTitle: otTitle })).toBe(false)
+  })
+
+  test(`jobTitle set as "${otTitle}" and onPremisesDistinguishedName includes "DC=skole" should be true`, () => {
+    expect(isOT({ onPremisesDistinguishedName: 'DC=skole', jobTitle: otTitle })).toBe(true)
   })
 })
 
@@ -72,20 +108,38 @@ describe('isStudent', () => {
     expect(isStudent()).toBe(false)
   })
 
-  test('type not set', () => {
+  test('title not set', () => {
     expect(isStudent({ displayName: 'Test' })).toBe(false)
   })
 
-  test('type set to an empty string', () => {
-    expect(isStudent({ type: '' })).toBe(false)
+  test('title set to an empty string', () => {
+    expect(isStudent({ title: '' })).toBe(false)
   })
 
-  test.each([apprenticeType, otType])('type set as %p should be false', type => {
-    expect(isStudent({ type })).toBe(false)
+  test.each([apprenticeTitle, otTitle])('title set as %p should be false', title => {
+    expect(isStudent({ title })).toBe(false)
   })
 
-  test(`type set as "${studentType}" should be true`, () => {
-    expect(isStudent({ type: studentType })).toBe(true)
+  test.each(['login', ''])(`domain set as %p and title set as "${studentTitle}" should be false`, domain => {
+    expect(isApprentice({ domain, title: studentTitle })).toBe(false)
+  })
+
+  test(`title set as "${studentTitle}" and domain set as "skole" should be true`, () => {
+    expect(isStudent({ domain: 'skole', title: studentTitle })).toBe(true)
+  })
+})
+
+describe('isStudent : AAD', () => {
+  test.each([otTitle, apprenticeTitle])('jobTitle set as %p should be false', jobTitle => {
+    expect(isStudent({ jobTitle })).toBe(false)
+  })
+
+  test.each(['DC=login', ''])(`onPremisesDistinguishedName includes %p and jobTitle set as "${studentTitle}" should be false`, onPremisesDistinguishedName => {
+    expect(isStudent({ onPremisesDistinguishedName, jobTitle: studentTitle })).toBe(false)
+  })
+
+  test(`jobTitle set as "${studentTitle}" and onPremisesDistinguishedName includes "DC=skole" should be true`, () => {
+    expect(isStudent({ onPremisesDistinguishedName: 'DC=skole', jobTitle: studentTitle })).toBe(true)
   })
 })
 
